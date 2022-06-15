f178ccb6ece156fe998be1e151e8987f45479d54
Alessandro Chumpitaz <par_214@hotmail.com>
2022-06-15 15:59:44 -0500

    it

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from helper.load_data import load
 from data import streets, intersections
+import graphviz as gv
 
 
 def main():
-    print(streets)
-    print(streets.find(4827828))
-    print(streets.find_by(name="Alfonso Ugarte"))
-    print(intersections.first)
+    print(intersections.first())
+    print(streets.find(intersections.first().street_id))
 
 
 if __name__ == "__main__":
--- models/intersections.py
+++ models/intersections.py
@@ -5,24 +5,28 @@ class Intersection:
                  origin_intersection_id, final_intersection_id, distance,
                  speed, cost_1, cost_2, origin_lat, origin_lon, final_lat,
                  final_lon):
-        identifier = int(identifier)
-        street_id = int(street_id)
-        street_name = street_name
-        origin_id = int(origin_id)
-        final_id = int(final_id)
-        origin_intersection_id = int(origin_intersection_id)
-        final_intersection_id = int(final_intersection_id)
-        distance = distance
-        speed = speed
-        cost_1 = cost_1
-        cost_2 = cost_2
-        origin_lat = origin_lat
-        origin_lon = origin_lon
-        final_lat = final_lat
-        final_lon = final_lon
+        self.identifier = int(identifier)
+        self.street_id = int(street_id)
+        self.street_name = street_name
+        self.origin_id = int(origin_id)
+        self.final_id = int(final_id)
+        self.origin_intersection_id = int(origin_intersection_id)
+        self.final_intersection_id = int(final_intersection_id)
+        self.distance = distance
+        self.speed = speed
+        self.cost_1 = cost_1
+        self.cost_2 = cost_2
+        self.origin_lat = origin_lat
+        self.origin_lon = origin_lon
+        self.final_lat = final_lat
+        self.final_lon = final_lon
 
-    def __len__(self):
-        return len(self.intersections)
+    def origin_coords(self):
+        return self.origin_lat, self.origin_lon
+
+    def final_coords(self):
+        return self.final_lat, self.final_lon
+
 
 
 class Intersections:
